58c251be40794cd9da69bf39b90b857f6a0cdbb4
liuxuejin <liuxuejin008@gmail.com>
2024-08-02 15:12:27 +0800

    flask试验模版001

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+from index import app
 
 if __name__ == '__main__':
-    import uvicorn
-    port = 8080  # 将端口号转换为整数
-    #railway启动
-    uvicorn.run(app='index:app', host="0.0.0.0", port=port)
-    #本地启动
-    #uvicorn.run(app='main:app', host="0.0.0.0", port=port)
+    app.run()
+
+
